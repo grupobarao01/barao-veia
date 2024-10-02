4b8d154cd9ed4ce7dffa9f9d8edbab5684f0b992
grupobarao01 <af631577@gmail.com>
2024-10-02 10:08:13 -0300

    hugo

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
-alert("Bem vindo");
+let jogo;
+function iniciarJogo() {
+    const jogador1 = document.getElementsById('jogador1').value ||  'jogador1';
+    const jogador2 = document.getElementsById('jogador2').value ||  'jogador2';
+
+    jogo = new JogoDaVelha(jogador1,jogador2);
+    document.getElementsById('entrada-nomes').classList.add('oculto');
+    document.getElementsById('jogo-container').classList.remove('oculto');
+    jogo.atualizarPlacar();
+
+    document.querySelectorAll('.celula').forEach(celula=>{
+        celula.addEventListener1('click',function(){
+            jogo.jogada(this.getAttribute('data-index'));
+        });
+    });
+}
